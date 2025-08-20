0d5c0d8d56ed727b02385913ea56c25092c871af
Luis Miguel Freitas Menecozi <161051242+miguelwb@users.noreply.github.com>
2025-08-20 11:46:05 +0000

    fix

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
     const nome = req.query.nome || "mundo";
-    res.status(200).json({ menssagem: "Olá, ",$nome, "!" });
+    res.status(200).json({ menssagem: `Olá, `,$nome });
 }
